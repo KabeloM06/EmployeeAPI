4ce4a8d27f14693477a8030d6f29d970129f4b00
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-17 08:58:39 +0200

    Add project files.

---
+++ EmployeeAPI/Data/CardsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Data
+{
+    public class CardsDbContext: DbContext
+    {
+        public CardsDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        //DbSet
+        // Acts as a table in SQLServer
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ EmployeeAPI/Models/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAPI.Models
+{
+    public class Card
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
